a6165f184effa1b8e803080ba2c0c441cbf4fee5
Man07har <thakurmanoharkumar7@gmail.com>
2023-10-10 01:15:33 +0530

    added products in product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Components from "./components/Components";
-import { ReactDOM } from "react";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Components from './components/Components';
+import { ReactDOM } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import "./App.css";
-import { ChakraProvider, Stack } from '@chakra-ui/react'
+import Home from './pages/Home';
+import './App.css';
+import { ChakraProvider, Stack } from '@chakra-ui/react';
 
-import { createStore } from "redux";
-import { Provider, useSelector, useDispatch, connect } from "react-redux";
-import MyCarousel from "./components/MyCarousel";
-import Categories from "./pages/Categories";
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+import MyCarousel from './components/MyCarousel';
+import Categories from './pages/Categories';
 
 // DOCS
 //  <Container /> will be used afterward and as a template when using redux
 
-
 const countReducer = (state = 0, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case 'INCREMENT':
       return state + 1;
 
-    case "DECREMENT":
+    case 'DECREMENT':
       0;
       return state - 1;
 
@@ -40,20 +39,20 @@ const MapStateToProps = (state) => {
 
 const MapDispatchToProps = (dispatch) => {
   return {
-    increment: () => dispatch({ type: "INCREMENT" }),
-    decrement: () => dispatch({ type: "DECREMENT" }),
+    increment: () => dispatch({ type: 'INCREMENT' }),
+    decrement: () => dispatch({ type: 'DECREMENT' }),
   };
 };
 
 const Component = ({ count, increment, decrement }) => {
   return (
     <>
-      <h1>Count = { count }</h1>
-      <button className="bg-blue-400" onClick={ increment }>
+      <h1>Count = {count}</h1>
+      <button className="bg-blue-400" onClick={increment}>
         Increment
       </button>
       <br />
-      <button className="bg-blue-200" onClick={ decrement }>
+      <button className="bg-blue-200" onClick={decrement}>
         Decrement
       </button>
     </>
@@ -65,23 +64,21 @@ const Container = connect(MapStateToProps, MapDispatchToProps)(Component);
 export default function App() {
   return (
     <>
-
       <ChakraProvider>
-        <Provider store={ store }>
+        <Provider store={store}>
           <div>
-            <Stack spacing={ 4 }>
+            <Stack spacing={4}>
               <Navbar />
               <Router>
                 <Routes>
-                  <Route path='/' element={ <Home /> } />
-                  <Route path='/categories' element={ <Categories /> } />
+                  <Route path="/" element={<Home />} />
+                  <Route path="/categories" element={<Categories />} />
                 </Routes>
               </Router>
               <Footer />
             </Stack>
           </div>
         </Provider>
-
       </ChakraProvider>
     </>
   );
