eea14e2961cc72ee0c00af291d1eb95bd3ca7edb
Kopal <yadavkopal436@gmail.com>
2023-10-12 21:41:17 +0530

    Added: last section of FAQ page

--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -1,11 +1,14 @@
-
 import React from 'react'
 import FAQhero from '../components/FAQhero'
+import FAQls from '../components/FAQls';
 
 const FAQ = () => {
   return (
     <>
+    <div className='bg-[#E7E1D8]'>
       <FAQhero />
+      <FAQls/>
+      </div>
     </>
   )
 }
