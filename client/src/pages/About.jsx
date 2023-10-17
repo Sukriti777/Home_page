a9473df4158fcd9a39dfb140997abb531fba38df
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-10-17 21:45:48 +0530

    add: the hero section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import AboutHero from '../components/AboutHero';
 
 function About() {
   return (
     <>
-      <div className="bg-[#FEA24D] w-screen p-[10em]">
-        <div className="border-8 border-[#FFC670] bg-[#FDFAF3] p-[8em] text-2xl font-medium rounded-lg flex justif-start">
-          Knothastags was born in 2023 out of a passion for helping our clients
-          bring their dream surprise events to life ! Since our creation, we
-          have committed ourselves to create impactful and creative experiences
-          for our clients and their guests , no matter the size of the event .
-          We always take into consideration the quality of the surprise event,
-          and never fall below the bar our clients set.
+      <AboutHero />
+      <div className="HISTORY OF KNOT">
+        <div className="bg-[#FEA24D] w-screen p-[10em]">
+          <div className="border-8 border-[#FFC670] bg-[#FDFAF3] p-[8em] text-2xl font-medium rounded-lg flex justif-start">
+            Knothastags was born in 2023 out of a passion for helping our clients
+            bring their dream surprise events to life ! Since our creation, we
+            have committed ourselves to create impactful and creative experiences
+            for our clients and their guests , no matter the size of the event .
+            We always take into consideration the quality of the surprise event,
+            and never fall below the bar our clients set.
+          </div>
         </div>
       </div>
+
     </>
   );
 }
