8a9ae63c0219ed258633b2e9baf46c7f8b4c79f8
Anupreet Singh <72339688+realanupreet@users.noreply.github.com>
2023-10-17 21:24:17 +0530

    refactor: readd lost code

--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -3,66 +3,72 @@ import FaqCard from '../components/FaqCard';
 import Bag from '../assets/bag.png';
 import Delivery from '../assets/truck.png';
 import Return from '../assets/return.png';
+import FAQhelp from '../components/FAQhelp';
+import FAQls from '../components/FAQls';
 
 const FAQ = () => {
   return (
     <>
       <div className="bg-[#E7E1D8]">
         <FAQhero />
-        <div className="grid justify-center">
-          <h1 className="text-2xl font-bold">Order Related Queries</h1>
-          <hr className="w-full text-black m-auto font-bold" />
+        <div className="ORDER AND VENDOR">
+          <div className="grid justify-center">
+            <h1 className="text-2xl font-bold">Order Related Queries</h1>
+            <hr className="w-full text-black m-auto font-bold" />
+          </div>
+          <div className=" flex m-2 justify-evenly 5s79">
+            <div className="text-2xl flex  bg-[#FFC587] ">
+              <FaqCard
+                image={ Bag }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+            <div className="text-2xl bg-[#FFC587]">
+              <FaqCard
+                image={ Delivery }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+            <div className="text-2xl bg-[#FFC587]">
+              <FaqCard
+                image={ Return }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+          </div>
+          <div className="grid justify-center">
+            <h1 className="text-2xl font-bold">Vendor Related Queries</h1>
+            <hr className="w-full text-black m-auto font-bold" />
+          </div>
+          <div className=" flex m-2 justify-evenly">
+            <div className="text-2xl bg-[#FFC587]">
+              <FaqCard
+                image={ Bag }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+            <div className="text-2xl bg-[#FFC587]">
+              <FaqCard
+                image={ Delivery }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+            <div className="text-2xl bg-[#FFC587]">
+              <FaqCard
+                image={ Return }
+                title="Products"
+                desc="View Product Information"
+              />
+            </div>
+          </div>
         </div>
-        <div className=" flex m-2 justify-evenly 5s79">
-          <div className="text-2xl flex  bg-[#FFC587] ">
-            <FaqCard
-              image={Bag}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-          <div className="text-2xl bg-[#FFC587]">
-            <FaqCard
-              image={Delivery}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-          <div className="text-2xl bg-[#FFC587]">
-            <FaqCard
-              image={Return}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-        </div>
-        <div className="grid justify-center">
-          <h1 className="text-2xl font-bold">Vendor Related Queries</h1>
-          <hr className="w-full text-black m-auto font-bold" />
-        </div>
-        <div className=" flex m-2 justify-evenly">
-          <div className="text-2xl bg-[#FFC587]">
-            <FaqCard
-              image={Bag}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-          <div className="text-2xl bg-[#FFC587]">
-            <FaqCard
-              image={Delivery}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-          <div className="text-2xl bg-[#FFC587]">
-            <FaqCard
-              image={Return}
-              title="Products"
-              desc="View Product Information"
-            />
-          </div>
-        </div>
+        <FAQhelp />
+        <FAQls />
       </div>
     </>
   );
