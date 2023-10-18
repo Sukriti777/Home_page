cd8c21de20d53755e09558435b5f2a3aba05c5b6
Kopal <yadavkopal436@gmail.com>
2023-10-19 01:13:19 +0530

    Revert "Added: founders and team section in about page"
    This reverts commit dd883f1a31fb56215fd2da3cb28f346a25f99e48.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,6 @@ export default {
     extend: {
       boxShadow: {
         custom: '10px 10px 0px 0px rgba(0, 0, 0, 0.5)', // Define your custom shadow here
-        'inner-thick': 'inset 8px 8px 4px 2px rgba(0, 0, 0, 0.4)',
       },
     },
   },
