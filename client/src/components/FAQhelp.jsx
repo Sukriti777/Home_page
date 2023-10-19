b09f312aa34801d965ab168e6f16203d0c1c5327
Sukriti777 <102283736+Sukriti777@users.noreply.github.com>
2023-10-20 04:02:13 +0530

    Minor changes on FAQ page
    Update FAQhelp.jsx file

--- client/src/components/FAQhelp.jsx
+++ client/src/components/FAQhelp.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Box, Center, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex, Text } from '@chakra-ui/react';
 import { FaShoppingBag, FaTruck } from 'react-icons/fa';
 
 const textStyle = {
   textAlign: 'center',
   color: 'black',
-  fontSize: '20px',
+  fontSize: '60px',
   fontWeight: 'light',
   fontFamily: 'Roboto',
   whiteSpace: 'nowrap',
@@ -16,115 +16,107 @@ const textStyle = {
 
 const ShoppingBox = () => {
   return (
-    <ChakraProvider>
+    <Box
+      w="750px"
+      h="350px"
+      backgroundColor="#FFB76B"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      style={{
+        boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.25)",
+        margin: "70px", // Add margin to create separation
+      }}
+    >
       <Box
-        w="150px"
-        h="130px"
-        backgroundColor="#FFB76B"
+        w="75%"
+        h="75%"
+        backgroundColor="#FFC587"
         display="flex"
         alignItems="center"
         justifyContent="center"
         style={{
-          boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.25)",
-          margin: "20px", // Increase the gap by adjusting the margin
+          boxShadow: "0 0 10px #FFC587",
+          backdropFilter: "blur(5px)",
+          position: "relative",
         }}
       >
-        <Box
-          w="75%"
-          h="75%"
-          backgroundColor="#FFC587"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          style={{
-            boxShadow: "0 0 10px #FFC587",
-            backdropFilter: "blur(5px)",
-            position: "relative",
-          }}
-        >
-          <Box style={{ position: "absolute", top: "0", left: "50%", transform: "translateX(-50%)" }}>
-            <FaShoppingBag size={24} color="black" />
-          </Box>
-
-          <Box sx={textStyle} style={{ marginTop: "30px" }}>
-            Shopping<br />With<br />Us
-          </Box>
+        <Box style={{ position: "absolute", top: "0", left: "50%", transform: "translateX(-50%)" }}>
+          <FaShoppingBag size={40} color="black" />
+        </Box>
+        <Box sx={textStyle} style={{ marginTop: "30px" }}>
+          Shopping<br />With<br />Us
         </Box>
       </Box>
-    </ChakraProvider>
+    </Box>
   );
 };
 
 const SellingBox = () => {
   return (
-    <ChakraProvider>
+    <Box
+      w="750px"
+      h="350px"
+      backgroundColor="#FFB76B"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      style={{
+        boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.25)",
+        margin: "70px", // Add margin to create separation
+      }}
+    >
       <Box
-        w="150px"
-        h="130px"
-        backgroundColor="#FFB76B"
+        w="75%"
+        h="75%"
+        backgroundColor="#FFC587"
         display="flex"
         alignItems="center"
         justifyContent="center"
         style={{
-          boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.25)",
-          margin: "20px", // Increase the gap by adjusting the margin
+          boxShadow: "0 0 10px #FFC587",
+          backdropFilter: "blur(5px)",
+          position: "relative",
         }}
       >
-        <Box
-          w="75%"
-          h="75%"
-          backgroundColor="#FFC587"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          style={{
-            boxShadow: "0 0 10px #FFC587",
-            backdropFilter: "blur(5px)",
-            position: "relative",
-          }}
-        >
-          <Box style={{ position: "absolute", top: "0", left: "50%", transform: "translateX(-50%)" }}>
-            <FaTruck size={24} color="black" />
-          </Box>
-
-          <Box sx={textStyle} style={{ marginTop: "30px" }}>
-            Selling<br />With<br />Us
-          </Box>
+        <Box style={{ position: "absolute", top: "0", left: "50%", transform: "translateX(-50%)" }}>
+          <FaTruck size={40} color="black" />
+        </Box>
+        <Box sx={textStyle} style={{ marginTop: "30px" }}>
+          Selling<br />With<br />Us
         </Box>
       </Box>
-    </ChakraProvider>
+    </Box>
   );
 };
 
 const App = () => {
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-    >
-      <Text
-        textAlign="center"
-        color="black"
-        fontSize="40px"
-        fontWeight="normal"
-        fontFamily="Roboto"
-        textDecoration="underline"
-      >
-        What Do You Need Help With?
-      </Text>
+    <ChakraProvider>
       <Flex
+        height="100vh"
         alignItems="center"
         justifyContent="center"
         flexDirection="column"
       >
-        <div style={{ display: 'flex' }}>
-          <ShoppingBox />
-          <SellingBox />
-        </div>
+        <Text
+          textAlign="center"
+          color="black"
+          fontSize="70px"
+          fontWeight="normal"
+          fontFamily="Roboto"
+          textDecoration="underline"
+        >
+          What Do You Need Help With?
+        </Text>
+        <Flex alignItems="center" justifyContent="center" flexDirection="column">
+          <div style={{ display: 'flex' }}>
+            <ShoppingBox />
+            <SellingBox />
+          </div>
+        </Flex>
       </Flex>
-    </Flex>
+    </ChakraProvider>
   );
 };
 
